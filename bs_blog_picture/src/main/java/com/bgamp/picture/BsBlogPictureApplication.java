329a0c6ce2c274885877fb8c09b1a1761db51a41
zb425 <1047441153@qq.com>
2020-05-12 21:53:08 +0800

    添加图片(picture)服务

---
+++ bs_blog_picture/src/main/java/com/bgamp/picture/BsBlogPictureApplication.java
@@ -0,0 +1,13 @@
+package com.bgamp.picture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsBlogPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsBlogPictureApplication.class, args);
+    }
+
+}
---
+++ bs_blog_picture/src/test/java/com/bgamp/picture/BsBlogPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgamp.picture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsBlogPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
