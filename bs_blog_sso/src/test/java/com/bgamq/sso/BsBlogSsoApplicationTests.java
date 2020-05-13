f50b85d884e5303c10aa248ba10e49895a823a61
sen588 <lisen19980328>
2020-05-13 14:01:43 +0800

    新增单点登录

---
+++ bs_blog_sso/src/test/java/com/bgamq/sso/BsBlogSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgamq.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsBlogSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
