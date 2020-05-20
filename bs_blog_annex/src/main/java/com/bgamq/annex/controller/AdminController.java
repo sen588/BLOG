aed758df65ebfaf9fb225b025171db0a4eb71417
sen588 <lisen19980328>
2020-05-20 16:32:44 +0800

    删除mapper，更新端口号

---
+++ bs_blog_annex/src/main/java/com/bgamq/annex/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.bgamq.annex.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/admin")
+public class AdminController {
+
+    @RequestMapping(value = "/get")
+    public String admin() {
+        return "annex";
+    }
+
+}
---
+++ bs_blog_sso/src/main/java/com/bgamq/sso/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.bgamq.sso.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/admin")
+public class AdminController {
+
+    @RequestMapping(value = "/get")
+    public String admin() {
+        return "sso";
+    }
+
+}
