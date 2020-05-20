e1c0f743a6d88992c971002dc69822008523d712
sen588 <lisen19980328>
2020-05-20 17:28:42 +0800

    jar包部署

--- bs_blog_core/src/main/java/com/bgamq/core/BsBlogCoreApplication.java
+++ bs_blog_core/src/main/java/com/bgamq/core/BsBlogCoreApplication.java
@@ -2,23 +2,11 @@ package com.bgamq.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BsBlogCoreApplication extends SpringBootServletInitializer {
+public class BsBlogCoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BsBlogCoreApplication.class, args);
-    }
-
-    /**
-     * 工程打成war需要继承SpringBootServletInitializer，重写configure方法
-     * @date 2018年12月7日
-     * @author zxp
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(BsBlogCoreApplication.class);
     }
 }
