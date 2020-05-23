9e0519735124a7207313af68046a80720d6739a7
zb425 <1047441153@qq.com>
2020-05-23 18:52:30 +0800

    修改网关配置 降低springboot版本

--- bs_blog_gateway/src/main/java/com/bgamq/gateway/BsBlogGatewayApplication.java
+++ bs_blog_gateway/src/main/java/com/bgamq/gateway/BsBlogGatewayApplication.java
@@ -1,31 +1,12 @@
 package com.bgamq.gateway;
 
-import com.bgamq.gateway.config.GatewayInterceptorConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BsBlogGatewayApplication implements WebMvcConfigurer {
-
-    @Value("${sso.excludes}")
-    public String excludes;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(gatewayInterceptorConfig())
-                .addPathPatterns("/**").excludePathPatterns(excludes);
-    }
-
-    @Bean
-    public GatewayInterceptorConfig gatewayInterceptorConfig() {
-        return new GatewayInterceptorConfig();
-    }
-
+@EnableDiscoveryClient
+public class BsBlogGatewayApplication{
     public static void main(String[] args) {
         SpringApplication.run(BsBlogGatewayApplication.class, args);
     }
